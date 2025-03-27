c809c0860f53ee6d716ebd8b3ed4f5441b03a7d8
Tetsuo Koyama <tkoyama010@gmail.com>
2025-03-28 07:46:05 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,7 +19,6 @@ basedir = Path(__file__).resolve().parent / "scientific-python-lectures"
 exec((basedir / "conf.py").read_text(), globals())  # noqa: S102
 locale_dirs = [basedir / "../locale/"]
 
-
 def setup(app) -> None:  # noqa: D103,ANN001
     app.srcdir = basedir
     app.confdir = app.srcdir
